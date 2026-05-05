3adee8d289a483754265e65c68210d9155cd1d02
Tusevo7 <104458576+Tusevo7@users.noreply.github.com>
2026-05-05 18:20:38 +0100

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -516,7 +516,7 @@ export default function App() {
 
       {/* WhatsApp Floating Button */}
       <a
-        href="https://wa.me/351XXXXXXXXX"
+        href="https://wa.me/+244947845946"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-[#25D366] text-white w-16 h-16 rounded-full shadow-lg hover:bg-[#20BA5A] transition-all hover:scale-110 flex items-center justify-center z-50 group"
